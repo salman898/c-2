c711eb84f11eafe449dd20bcb849423eff7669af
salman898 <s.t.khan701@gmail.com>
2021-03-21 16:23:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Salman Khan......</h1>
+      <h1>Hello Salman Khan ......</h1>
+      <p>Class-02 Asignment Complete And the error which is created again and 
+        again was just the problem with permission where i used "Sudo" before 
+        yarn build command "sudo yarn Build"
+      </p>
     </div>
   );
 }
